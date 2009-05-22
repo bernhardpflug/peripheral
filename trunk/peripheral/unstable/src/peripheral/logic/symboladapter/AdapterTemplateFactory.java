c84792166972ae04d22d1bfd6f5b1ce45d3637b3
bernhard.pflug@gmail.com <bernhard.pflug@gmail.com@40046408-4061-11de-9687-fb973330207e>
2009-05-22 16:34:26 +0000

    dummy templates

--- trunk/peripheral/unstable/src/peripheral/logic/symboladapter/AdapterTemplateFactory.java
+++ trunk/peripheral/unstable/src/peripheral/logic/symboladapter/AdapterTemplateFactory.java
@@ -1,15 +1,57 @@
 package peripheral.logic.symboladapter;
 
+import java.util.ArrayList;
+import peripheral.logic.value.ConstValue;
+import peripheral.logic.value.SensorValue;
+import peripheral.logic.value.UserInput;
+
 
 public class AdapterTemplateFactory {
+
+    private static AdapterTemplateFactory instance;
 
     private java.util.List<SymbolAdapter> templates;
 
     private AdapterTemplateFactory () {
+
+        templates = new ArrayList<SymbolAdapter>();
+
+        //TODO just for testing purposes; replace by real templates
+        createDummyTemplates();
+    }
+
+    private void createDummyTemplates() {
+
+        SymbolAdapter contSlider = new SymbolAdapter();
+        contSlider.setName("continuousSlider");
+        contSlider.setDescription("A slider based on \na sensorvalue that continously \nmaps the sensorvalue\n onto a selected area");
+        contSlider.getNeededUserInput().add(new UserInput("ui1","what the hell", new SensorValue("SensorValue",null)));
+        contSlider.getNeededUserInput().add(new UserInput("ui2","what the hell", new ConstValue("LocationX",new Integer(0))));
+        contSlider.getNeededUserInput().add(new UserInput("ui2","what the hell", new ConstValue("LocationY",new Integer(0))));
+
+        contSlider.getRequiredSteps().put(peripheral.designer.wizard.AddAnimationDialog.RULEPANEL, new Boolean(false));
+
+        templates.add(contSlider);
+
+        SymbolAdapter ruleSlider = new SymbolAdapter();
+        ruleSlider.setName("ruleBasedSlider");
+        ruleSlider.setDescription("A slider were rules \ncan be defined for mapping \nseveral sensorvalues onto\n the position of the picture");
+        ruleSlider.getNeededUserInput().add(new UserInput("ui1","what the hell", new ConstValue("EnableSmoothing",new Boolean(true))));
+        ruleSlider.getNeededUserInput().add(new UserInput("ui2","what the hell", new ConstValue("LocationX",new Integer(0))));
+        ruleSlider.getNeededUserInput().add(new UserInput("ui2","what the hell", new ConstValue("LocationY",new Integer(0))));
+
+        ruleSlider.getRequiredSteps().put(peripheral.designer.wizard.AddAnimationDialog.RULEPANEL, new Boolean(true));
+
+        templates.add(ruleSlider);
     }
 
     public static AdapterTemplateFactory getInstance () {
-        return null;
+
+        if (instance == null) {
+            instance = new AdapterTemplateFactory();
+        }
+
+        return instance;
     }
 
     public java.util.List<SymbolAdapter> getTemplates () {
