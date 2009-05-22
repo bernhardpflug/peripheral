3a95389aea631ddf2c8557acb683dba48b42a4b0
tobias.noiges <tobias.noiges@40046408-4061-11de-9687-fb973330207e>
2009-05-22 16:18:36 +0000

    Instances can now be initialized by XML derived from jddacserver

---
+++ trunk/peripheral/unstable/src/peripheral/logic/sensor/CreateInstancesFromServerXML.java
@@ -0,0 +1,32 @@
+package peripheral.logic.sensor;
+
+import java.util.TreeMap;
+
+
+public class CreateInstancesFromServerXML {
+
+	public static void main(String[] args) {
+		
+		// Create Server
+		SensorServer server = new SensorServer("http://127.0.0.1",8080, "admin");
+		
+		// Get Server's XML Parser
+		XmlMetaParser xml = server.getXmlMetaParser();
+		xml.createInstancesFromXML();
+
+		// Check count of sensors
+		System.out.println("Server has " + server.getSensorList().size() + " Sensors.");
+		
+		for(Sensor sensor : server.getSensorList()){
+			System.out.println("Sensor " + sensor.getName() + " with PID " + sensor.getPid()
+					+ " has " + sensor.getSensorChannels().size() + " Channels:");
+			
+			for(SensorChannel channel : sensor.getSensorChannels()){
+				System.out.println("Channel " + channel.getFullname() + " with MID " + channel.getMid()
+						+ " with " + channel.getMetadata().size() + "Metatags.");
+			}
+			
+		}
+	}
+
+}
