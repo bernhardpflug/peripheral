bf3ca9690931a33ac40ac3f78afcd044da242500
tobias.noiges <tobias.noiges@40046408-4061-11de-9687-fb973330207e>
2009-05-22 16:18:36 +0000

    Instances can now be initialized by XML derived from jddacserver

--- peripheral/unstable/src/peripheral/logic/sensor/Sensor.java
+++ peripheral/unstable/src/peripheral/logic/sensor/Sensor.java
@@ -1,7 +1,6 @@
 package peripheral.logic.sensor;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Observable;
 
 
--- peripheral/unstable/src/peripheral/logic/sensor/XmlMetaParser.java
+++ peripheral/unstable/src/peripheral/logic/sensor/XmlMetaParser.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.TreeMap;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -19,6 +20,7 @@ import org.xml.sax.SAXException;
 public class XmlMetaParser {
 
     private SensorServer server;
+    private Sensor tempSensor;
 
     public XmlMetaParser (SensorServer server) {
     	this.server = server;
@@ -27,7 +29,7 @@ public class XmlMetaParser {
     public void createInstancesFromXML () {
     	
     	DataInputStream xml = getXmlFromServer();
-    	parseXml(xml);
+    	parseXmlAndCreateInstances(xml);
     }
     
     protected DataInputStream getXmlFromServer(){
@@ -55,7 +57,7 @@ public class XmlMetaParser {
     	return is;
     }
     
-    protected void parseXml(DataInputStream xmlInputStream){
+    protected void parseXmlAndCreateInstances(DataInputStream xmlInputStream){
 		
 		try {
 			
@@ -63,37 +65,123 @@ public class XmlMetaParser {
 			DocumentBuilder builder = factory.newDocumentBuilder();
 			Document document = builder.parse((xmlInputStream));
 			
+			// Get Sensor tags
 			NodeList sensors = document.getElementsByTagName("sensor");
 			
-			for(int k = 0; k<sensors.getLength();k++){
+			// Iterate through sensor nodes
+			for(int i = 0; i<sensors.getLength();i++){
 				
-				System.out.println("<sensor>");
+				// Create empty Sensor
+				Sensor sensor;
 				
-				NodeList children = sensors.item(k).getChildNodes();
+				// Get child nodes of sensornode
+				NodeList s_children = sensors.item(i).getChildNodes();
 				
-				// Check for Channels first
-				for(int i = 0; i<children.getLength(); i++){
-					if(children.item(i).getNodeType() == Node.ELEMENT_NODE && children.item(i).getNodeName().compareTo("channel")==0){
+				// Create attributes for Sensor constructor
+				String sensorname = "";
+				String pid = "";
+				
+				// Create Sensor Instance
+				for(int j = 0; j<s_children.getLength(); j++){
+					
+					// Get Sensor name
+					if(s_children.item(j).getNodeType()==Node.ELEMENT_NODE && s_children.item(j).getNodeName().compareTo("name")==0){
+						sensorname = s_children.item(j).getTextContent();
+					}
+					
+					if(s_children.item(j).getNodeType()==Node.ELEMENT_NODE && s_children.item(j).getNodeName().compareTo("pid")==0){
+						pid = s_children.item(j).getTextContent();
+					}
+					
+					if(sensorname.compareTo("")!=0 && pid.compareTo("")!=0)
+						break;
+				}
+				
+				// Create Sensor instance
+				if(sensorname.compareTo("")!=0 && pid.compareTo("")!=0){
+					tempSensor = new Sensor(Long.parseLong(pid), sensorname, server, 1.0f);
+				}
+				
+				// Create Channels if available
+				for(int j=0; j<s_children.getLength();j++){
+					if(s_children.item(j).getNodeType() == Node.ELEMENT_NODE && s_children.item(j).getNodeName().compareTo("channel")==0){
 						
-						NodeList channels = children.item(i).getChildNodes();
+						// Get child nodes of channel node
+						NodeList c_children = s_children.item(j).getChildNodes();
 						
-						for(int j = 0; j<channels.getLength(); j++){
-							if(channels.item(j).getNodeType() == Node.ELEMENT_NODE){
-								System.out.println(channels.item(j).getNodeName() + ": " + channels.item(j).getTextContent());
+						// Attributes for channel constructor
+						String mid = "";
+						String fullname = "";
+						
+						// Iterate through children of channel node to get mid and fullname
+						for(int k = 0; k<c_children.getLength(); k++){
+							
+							// Get mid
+							if(c_children.item(k).getNodeType()==Node.ELEMENT_NODE && c_children.item(k).getNodeName().compareTo("mid")==0){
+								mid = c_children.item(k).getTextContent();
+							}
+							
+							// Get fullname
+							if(c_children.item(k).getNodeType()==Node.ELEMENT_NODE && c_children.item(k).getNodeName().compareTo("fullname")==0){
+								fullname = c_children.item(k).getTextContent();
+							}
+							
+							if(mid.compareTo("")!=0 && fullname.compareTo("")!=0)
+								break;
+						}
+						
+						// Create Channel
+						SensorChannel channel = new SensorChannel(Long.parseLong(mid), fullname, tempSensor);
+						
+						// Add Metadata to Channel
+						TreeMap<String, String> metadata = channel.getMetadata();
+						
+						// Iterate through channel's children to generate metadata
+						for(int k = 0; k<c_children.getLength(); k++){
+							
+							// Get shortname
+							if(c_children.item(k).getNodeType()==Node.ELEMENT_NODE && c_children.item(k).getNodeName().compareTo("shortname")==0){
+								metadata.put("shortname", c_children.item(k).getTextContent());
+							}
+							
+							// Get description
+							if(c_children.item(k).getNodeType()==Node.ELEMENT_NODE && c_children.item(k).getNodeName().compareTo("description")==0){
+								metadata.put("description", c_children.item(k).getTextContent());
+							}
+							
+							// Get datatype
+							if(c_children.item(k).getNodeType()==Node.ELEMENT_NODE && c_children.item(k).getNodeName().compareTo("datatype")==0){
+								metadata.put("datatype", c_children.item(k).getTextContent());
+							}
+							
+							// Get units
+							if(c_children.item(k).getNodeType()==Node.ELEMENT_NODE && c_children.item(k).getNodeName().compareTo("units")==0){
+								metadata.put("units", c_children.item(k).getTextContent());
+							}
+							
+							// Get location
+							if(c_children.item(k).getNodeType()==Node.ELEMENT_NODE && c_children.item(k).getNodeName().compareTo("location")==0){
+								metadata.put("location", c_children.item(k).getTextContent());
+							}
+							
+							// Get upperlimit
+							if(c_children.item(k).getNodeType()==Node.ELEMENT_NODE && c_children.item(k).getNodeName().compareTo("upperlimit")==0){
+								metadata.put("upperlimit", c_children.item(k).getTextContent());
+							}
+							
+							// Get lowerlimit
+							if(c_children.item(k).getNodeType()==Node.ELEMENT_NODE && c_children.item(k).getNodeName().compareTo("lowerlimit")==0){
+								metadata.put("lowerlimit", c_children.item(k).getTextContent());
 							}
 						}
 						
+						// Add channel to sensor
+						tempSensor.getSensorChannels().add(channel);
 					}
 				}
 				
-				// Print attributes
-				for(int i = 0; i<children.getLength(); i++){
-					if(children.item(i).getNodeType() == Node.ELEMENT_NODE && children.item(i).getNodeName().compareTo("channel")!=0){
-						System.out.println(children.item(i).getNodeName() + ": " + children.item(i).getTextContent());
-					}
-				}
-				
-				System.out.println("</sensor>\n");
+				// Add sensor to server
+				server.getSensorList().add(tempSensor);
 			}
 			
 		} catch (ParserConfigurationException e) {
