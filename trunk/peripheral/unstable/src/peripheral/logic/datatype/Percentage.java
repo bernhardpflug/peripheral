c1cd477bb966167032a0ccc799d20915ccb86290
a.sippl@gmx.at <a.sippl@gmx.at@40046408-4061-11de-9687-fb973330207e>
2009-05-26 16:14:53 +0000

    new datatype

---
+++ trunk/peripheral/unstable/src/peripheral/logic/datatype/Percentage.java
@@ -0,0 +1,29 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package peripheral.logic.datatype;
+
+/**
+ *
+ * @author Andi
+ */
+public class Percentage {
+    private double val = 0;
+
+    public Percentage(double val) {
+        this.val = val;
+    }
+
+    public Percentage() {
+    }
+
+    public double getVal() {
+        return val;
+    }
+
+    public void setVal(double val) {
+        this.val = val;
+    }
+}
