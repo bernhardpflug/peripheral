8b057c0f1c47fe805d706a1ebf18353f87058011
bernhard.pflug@gmail.com <bernhard.pflug@gmail.com@40046408-4061-11de-9687-fb973330207e>
2009-05-27 11:56:41 +0000

    for line and point full drag support, dragging only allowed in set location set, smaller bugfixes

--- trunk/peripheral/unstable/src/peripheral/logic/positioningtool/Line.java
+++ trunk/peripheral/unstable/src/peripheral/logic/positioningtool/Line.java
@@ -2,6 +2,8 @@ package peripheral.logic.positioningtool;
 
 import java.awt.Graphics;
 import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+import peripheral.logic.symboladapter.Symbol;
 
 
 public class Line extends PositioningTool {
@@ -11,12 +13,20 @@ public class Line extends PositioningTool {
     private java.awt.Point endPoint;
 
     private static final int CROSSRANGE = 10;
+    
+    //reference to either start- or endpoint to define point for manipulation
+    private java.awt.Point draggingPoint;
+
+    //offset between origin of symbol and current mouse cursor
+    //(otherwise first drag creates a jump if click was far away from origin)
+    private java.awt.Point dragOffset;
 
     public Line () {
         super();
 
         startPoint = new java.awt.Point();
         endPoint = new java.awt.Point();
+        dragOffset = new java.awt.Point();
 
         startPoint.x = 6;
         startPoint.y = 6;
@@ -62,42 +72,122 @@ public class Line extends PositioningTool {
         //in case of painting symbol paint for symbol
         if (super.getDisplayedSymbol()!=null && super.isSymbolDisplayed()) {
 
+            Symbol symbol = super.getDisplayedSymbol();
+
+            //paint symbol with scale and imageBounds
+
+            BufferedImage symbolImage = symbol.getBufferedImage();
+
+            //ATTENTION: IF CHANGING DISPLAY FROM STARTPOINT HERE ALSO CHANGE IN DRAGABLE
+            Rectangle symbolBounds = getSymbolBounds(symbolImage, scale, startPoint, imageBounds);
+
+            g.drawImage(symbolImage, (int) symbolBounds.getX(), (int) symbolBounds.getY(), (int) symbolBounds.getX() + (int) symbolBounds.getWidth(), (int) symbolBounds.getY() + (int) symbolBounds.getHeight(),
+                    0, 0, symbolImage.getWidth(), symbolImage.getHeight(), null, null);
         }
-        else {
-            //paint line with crosses at begin and endpoint
 
-            //draw line
-            g.drawLine(scaledStartX, scaledStartY, scaledEndX, scaledEndY);
+        
+        //paint line with crosses at begin and endpoint
 
-            //draw cross startPoint
-            g.drawLine(scaledStartX-(int)(CROSSRANGE*scale), scaledStartY-(int)(CROSSRANGE*scale), scaledStartX+(int)(CROSSRANGE*scale), scaledStartY+(int)(CROSSRANGE*scale));
-            g.drawLine(scaledStartX-(int)(CROSSRANGE*scale), scaledStartY+(int)(CROSSRANGE*scale), scaledStartX+(int)(CROSSRANGE*scale), scaledStartY-(int)(CROSSRANGE*scale));
+        //draw line
+        g.drawLine(scaledStartX, scaledStartY, scaledEndX, scaledEndY);
 
-            //draw cross endPoint
-            g.drawLine(scaledEndX-(int)(CROSSRANGE*scale), scaledEndY-(int)(CROSSRANGE*scale), scaledEndX+(int)(CROSSRANGE*scale), scaledEndY+(int)(CROSSRANGE*scale));
-            g.drawLine(scaledEndX-(int)(CROSSRANGE*scale), scaledEndY+(int)(CROSSRANGE*scale), scaledEndX+(int)(CROSSRANGE*scale), scaledEndY-(int)(CROSSRANGE*scale));
+        //draw cross startPoint
+        g.drawLine(scaledStartX - (int) (CROSSRANGE * scale), scaledStartY - (int) (CROSSRANGE * scale), scaledStartX + (int) (CROSSRANGE * scale), scaledStartY + (int) (CROSSRANGE * scale));
+        g.drawLine(scaledStartX - (int) (CROSSRANGE * scale), scaledStartY + (int) (CROSSRANGE * scale), scaledStartX + (int) (CROSSRANGE * scale), scaledStartY - (int) (CROSSRANGE * scale));
 
-        }
+        //draw cross endPoint
+        g.drawLine(scaledEndX - (int) (CROSSRANGE * scale), scaledEndY - (int) (CROSSRANGE * scale), scaledEndX + (int) (CROSSRANGE * scale), scaledEndY + (int) (CROSSRANGE * scale));
+        g.drawLine(scaledEndX-(int)(CROSSRANGE*scale), scaledEndY+(int)(CROSSRANGE*scale), scaledEndX+(int)(CROSSRANGE*scale), scaledEndY-(int)(CROSSRANGE*scale));
+
     }
 
     @Override
     public boolean dragable(int x, int y) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        
+        //if internal method returns whether start-or endpoint return true
+        if (internalDragable(x,y) != null) {
+            return true;
+        }
+        //if no point is returned drag is not possible
+        else {
+            return false;
+        }
+    }
+
+
+    /**
+     * internal method that checks for dragable areas and returns either the
+     * start-, endpoint if dragable and null else
+     * @param x background image coordinates to check drag for
+     * @param y background image coordinates to check drag for
+     * @return either start-endpoint if dragable or null if not
+     */
+    private java.awt.Point internalDragable(int x, int y) {
+
+        //check for both startpoint and endpoint to drag
+
+        //in case symbol is displayed size of symbol is drag area for startpoint
+        if (super.getDisplayedSymbol() != null && super.isSymbolDisplayed()) {
+
+            Symbol symbol = super.getDisplayedSymbol();
+
+            if (x >= startPoint.x && x <= (startPoint.x + symbol.getBufferedImage().getWidth()) &&
+                    y >= startPoint.y && y <= (startPoint.y + symbol.getBufferedImage().getHeight())) {
+
+                return startPoint;
+            }
+
+        } //cross is drag area for startpoint
+        else {
+            //if startpoint is displayed as cross define cross range as drag area
+
+            if (x >= startPoint.x - CROSSRANGE && x <= startPoint.x + CROSSRANGE &&
+                    y >= startPoint.y - CROSSRANGE && y <= startPoint.y + CROSSRANGE) {
+
+                return startPoint;
+            }
+        }
+
+        //check endpoint (which is always represented as cross for dragable area
+        if (x >= endPoint.x - CROSSRANGE && x <= endPoint.x + CROSSRANGE &&
+                y >= endPoint.y - CROSSRANGE && y <= endPoint.y + CROSSRANGE) {
+
+            return endPoint;
+        }
+
+        //if non of the above conditions is suitable area is not dragable
+        return null;
     }
 
     @Override
     public void dragStart(java.awt.Point origin) {
-        throw new UnsupportedOperationException("Not supported yet.");
+
+        //in here remember whether start- or endpoint clicked
+        if (draggingPoint == null) {
+
+            draggingPoint = internalDragable(origin.x, origin.y);
+
+            if (draggingPoint != null) {
+
+                //save offset between current curser and origin
+                dragOffset.x = origin.x - draggingPoint.x;
+                dragOffset.y = origin.y - draggingPoint.y;
+            }
+        }
     }
 
     @Override
     public void dragAction(java.awt.Point newPosition) {
-        throw new UnsupportedOperationException("Not supported yet.");
+
+        if (draggingPoint != null) {
+            this.draggingPoint.x = newPosition.x - dragOffset.x;
+            this.draggingPoint.y = newPosition.y - dragOffset.y;
+        }
     }
 
     @Override
     public void dragStop() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        draggingPoint = null;
     }
 
 }
