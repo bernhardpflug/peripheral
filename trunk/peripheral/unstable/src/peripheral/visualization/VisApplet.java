e65d372647a2c430045b1f8899b9ff9875c2eaa2
a.sippl@gmx.at <a.sippl@gmx.at@40046408-4061-11de-9687-fb973330207e>
2009-05-27 14:40:48 +0000

    Made VisApplet starting point of application

--- trunk/peripheral/unstable/src/peripheral/visualization/VisApplet.java
+++ trunk/peripheral/unstable/src/peripheral/visualization/VisApplet.java
@@ -1,6 +1,5 @@
 package peripheral.visualization;
 
-
 import java.awt.Dimension;
 import java.util.Vector;
 import peripheral.logic.positioningtool.Region;
@@ -10,122 +9,126 @@ import processing.core.PImage;
 
 public class VisApplet extends PApplet implements Visualization {
 
-	private static final long serialVersionUID = 1L; //	whatever that's needed for
-	private Vector<VisSymbol> symbols;
-	private PImage bgImage;
-	private Dimension screen;
-	static private Visualization instance;
-	
-	private VisApplet(){
-		symbols = new Vector<VisSymbol>();
-	}
-	
-	static public Visualization getInstance(){
-		if (instance == null){
-			instance = new VisApplet();
-		}
-		return instance;
-	}
+    private static final long serialVersionUID = 1L; //	whatever that's needed for
+    private Vector<VisSymbol> symbols;
+    private PImage bgImage;
+    private Dimension screen;
 
-	public void setup()
-	{
-		size((int)screen.getWidth(),(int)screen.getHeight(), P3D);
-		frameRate(60);
-		noStroke();
-	}
-	
-	/*public void mouseClicked(){	//used for the mover demo
-		
-	}*/
+    public VisApplet() {
+        symbols = new Vector<VisSymbol>();
+    }
 
-	public void draw()
-	{
-		background(bgImage);
-		VisSymbol ptr;
-		for (int i = 0; i < symbols.size(); i++){
-			ptr = symbols.get(i);
-			ptr.calcStep();
-			//brightness and alpha
-			tint((255.f * ptr.getBrightness()), (255.f * ptr.getAlpha()));
-			if (ptr.getImg() != null)
-				image(ptr.getImg(), (float)ptr.getPositionIpl().getX(), (float)ptr.getPositionIpl().getY());
-			
-			//img swapping active:
-			if (ptr.getImgSwap() != null){
-				tint((255.f * ptr.getBrightness()), (255.f * ptr.getAlphaSwap()));
-				image(ptr.getImgSwap(), (float)ptr.getPositionIpl().getX(), (float)ptr.getPositionIpl().getY());
-			}
-			
-		}
-		
-	}
-	
-	public void add(Symbol s, Region region) {
-		VisSymbol vs = new VisSymbol(s, region);
-		vs.setImg(loadImage(s.getFile().getPath()));
-		vs.setFadeIn(true);
-		symbols.add(vs);
-	}
 
-	public void brightness(Symbol s, float amount) {
-		VisSymbol vs = findVSforS(s);
-		vs.setBrightness(amount);
-	}
+    public void setup() {
+        peripheral.logic.Runtime.getInstance().startup(this, "displayConfig.ser");
+   
+        size((int) screen.getWidth(), (int) screen.getHeight(), P3D);
+        frameRate(60);
+        noStroke();
+    }
 
-	public void contrast(Symbol s, float amount) {
-		// TODO Auto-generated method stub
-		
-	}
+    /*public void mouseClicked(){	//used for the mover demo
 
-	public void hide(Symbol s) {
-		VisSymbol vs = findVSforS(s);
-		vs.setVisible(false);	
-	}
+    }*/
+    public void draw() {
+        //background(bgImage);
+        background(255);
+        this.color(0);
+        this.rect(100, 100, 200, 200);
+        VisSymbol ptr;
+        for (int i = 0; i < symbols.size(); i++) {
+            ptr = symbols.get(i);
+            ptr.calcStep();
+            //brightness and alpha
+            tint((255.f / ptr.getBrightness()), (255.f / ptr.getAlpha()));
+            image(ptr.getImg(), (float) ptr.getPositionIpl().getX(), (float) ptr.getPositionIpl().getY());
+        }
 
-	public void remove(Symbol s) {
-		VisSymbol vs = findVSforS(s);
-		symbols.remove(vs);
-		
-	}
+    }
 
-	public void rotate(Symbol s, float angle) {
-		s.setAngle(angle);
-	}
+    public void add(Symbol s, Region region) {
+        VisSymbol vs = new VisSymbol(s, region);
+        vs.setImg(loadImage(s.getFile().getPath()));
+        vs.setFadeIn(true);
+        symbols.add(vs);
+    }
 
-	public void scale(Symbol s, float factorX, float factorY) {
-		s.setScaleX(factorX);
-		s.setScaleY(factorY);
-	}
+    public void brightness(Symbol s, float amount) {
+        VisSymbol vs = findVSforS(s);
+        vs.setBrightness(amount);
+    }
 
-	public void show(Symbol s) {
-		VisSymbol vs = findVSforS(s);
-		vs.setVisible(true);
-		
-	}
+    public void contrast(Symbol s, float amount) {
+        // TODO Auto-generated method stub
+    }
 
-	public void swap(Symbol s, String filename) {
-		VisSymbol vs = findVSforS(s);
-		PImage temp = loadImage(filename);
-		vs.setImgSwap(temp);
-	}
-	
-	private VisSymbol findVSforS(Symbol s){
-		for (int i = 0; i < symbols.size(); i++){
-			VisSymbol vs = symbols.get(i);
-			if (vs.getSymbol() == s)
-				return vs;
-		}
-		System.out.println("findVSforS: no suitable VisSymbol found!");
-		return null;
-	}
+    public void hide(Symbol s) {
+        VisSymbol vs = findVSforS(s);
+        vs.setVisible(false);
+    }
 
-	public void init(String backgroundImageFilename, Dimension resolution) {
-		bgImage = loadImage(backgroundImageFilename);
-		screen = resolution;
-	}
+    public void remove(Symbol s) {
+        VisSymbol vs = findVSforS(s);
+        symbols.remove(vs);
 
-	public void translate(Symbol s, java.awt.Point targetPosition) {
-		s.getPosition().setPosition(targetPosition);
-	}
+    }
 
+    public void rotate(Symbol s, float angle) {
+        s.setAngle(angle);
+    }
+
+    public void scale(Symbol s, float factorX, float factorY) {
+        s.setScaleX(factorX);
+        s.setScaleY(factorY);
+    }
+
+    public void show(Symbol s) {
+        VisSymbol vs = findVSforS(s);
+        vs.setVisible(true);
+
+    }
+
+    public void swap(Symbol s, String filename) {
+        // TODO Auto-generated method stub
+    }
+
+    private VisSymbol findVSforS(Symbol s) {
+        for (int i = 0; i < symbols.size(); i++) {
+            VisSymbol vs = symbols.get(i);
+            if (vs.getSymbol() == s) {
+                return vs;
+            }
+        }
+        return null;
+    }
+
+    public void init(String backgroundImageFilename, Dimension resolution) {
+
+        bgImage = loadImage(backgroundImageFilename);
+        screen = resolution;
+    }
+
+    public void translate(Symbol s, java.awt.Point targetPosition) {
+        s.getPosition().setPosition(targetPosition);
+
+    }
+
+     /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //@todo: read configuration-filename from args
+
+        //Runtime.getInstance().startup("displayConfig.ser");
+        PApplet.main(new String[]{"--present", "peripheral.visualization.VisApplet"});
+
+        try {
+            System.out.println("Press <Enter> to terminate visualization.");
+            System.in.read();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        //Runtime.getInstance().shutdown();
+    }
 }
