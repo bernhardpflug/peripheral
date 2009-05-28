21e120e5c63c7b580f45a611f3662a4eb5d6f310
a.sippl@gmx.at <a.sippl@gmx.at@40046408-4061-11de-9687-fb973330207e>
2009-05-28 12:45:39 +0000

    removed Generic Type --> doesn't need it initialize lists

--- peripheral/unstable/src/peripheral/logic/positioningtool/ToolList.java
+++ peripheral/unstable/src/peripheral/logic/positioningtool/ToolList.java
@@ -3,34 +3,30 @@ package peripheral.logic.positioningtool;
 import java.util.ArrayList;
 
 
-public class ToolList <T extends PositioningTool> implements ActionTool {
+public class ToolList implements ActionTool {
 
-    private java.util.ArrayList<T> visibleElements;
+    private java.util.ArrayList<PositioningTool> visibleElements;
 
-    private java.util.ArrayList<T> hiddenElements;
-
-    private T type;
-
-    public T getType() {
-        return type;
-    }
+    private java.util.ArrayList<PositioningTool> hiddenElements;
 
     public ToolList () {
+        visibleElements = new ArrayList<PositioningTool>();
+        hiddenElements = new ArrayList<PositioningTool>();
     }
 
-    public java.util.ArrayList<T> getHiddenElements () {
+    public java.util.ArrayList<PositioningTool> getHiddenElements () {
         return hiddenElements;
     }
 
-    public void setHiddenElements (java.util.ArrayList<T> val) {
+    public void setHiddenElements (java.util.ArrayList<PositioningTool> val) {
         this.hiddenElements = val;
     }
 
-    public java.util.ArrayList<T> getVisibleElements () {
+    public java.util.ArrayList<PositioningTool> getVisibleElements () {
         return visibleElements;
     }
 
-    public void setVisibleElements (java.util.ArrayList<T> val) {
+    public void setVisibleElements (java.util.ArrayList<PositioningTool> val) {
         this.visibleElements = val;
     }
 
