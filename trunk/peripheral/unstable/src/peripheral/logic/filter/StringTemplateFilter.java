83e61fc95d6e32d114eea1f3c3af3ce7e762f3f7
a.sippl@gmx.at <a.sippl@gmx.at@40046408-4061-11de-9687-fb973330207e>
2009-05-28 09:51:42 +0000

    implemented filtering

--- trunk/peripheral/unstable/src/peripheral/logic/filter/StringTemplateFilter.java
+++ trunk/peripheral/unstable/src/peripheral/logic/filter/StringTemplateFilter.java
@@ -2,6 +2,7 @@ package peripheral.logic.filter;
 
 import java.util.ArrayList; 
 import peripheral.logic.symboladapter.SymbolAdapter;
+import peripheral.logic.value.ConstValue;
 import peripheral.logic.value.Value; 
 
 public class StringTemplateFilter extends Filter {
@@ -14,7 +15,16 @@ public class StringTemplateFilter extends Filter {
 
     @Override
     public Object doFilter () {
-        return null;
+        String template = this.template.getValue().toString();
+        System.out.println("StringTemplateFilter, template=" + template);
+
+        template = template.replaceAll("###VAL###", this.getInputValue().toString());
+
+        new ConstValue(adapter, this.getOutputVarName(), template);
+
+        System.out.println("StringTemplateFilter, templateAfterReplace=" + template);
+
+        return template;
     }
 
     public ArrayList<java.lang.Class> getAcceptedInputTypes () {
