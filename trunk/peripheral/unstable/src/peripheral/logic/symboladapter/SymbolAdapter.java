007f4ee738fd207ed510b4328fb4b6664ab31afe
a.sippl@gmx.at <a.sippl@gmx.at@40046408-4061-11de-9687-fb973330207e>
2009-05-28 14:01:02 +0000

    made SymbolAdapter cloneable --> createCopy()

--- trunk/peripheral/unstable/src/peripheral/logic/symboladapter/SymbolAdapter.java
+++ trunk/peripheral/unstable/src/peripheral/logic/symboladapter/SymbolAdapter.java
@@ -1,105 +1,97 @@
 package peripheral.logic.symboladapter;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Map;
 import peripheral.logic.action.Action;
 import peripheral.logic.filter.Filter;
-import peripheral.logic.positioningtool.ActionTool; 
+import peripheral.logic.positioningtool.ActionTool;
 import peripheral.logic.rule.Rule;
 import peripheral.logic.sensor.Sensor;
 import peripheral.logic.value.UserInput;
 import peripheral.logic.value.Value;
 
-public class SymbolAdapter implements Serializable{
+public class SymbolAdapter implements Serializable {
 
     public enum RequiredStep implements Serializable {
+
         Rules
     }
-
     private ActionTool tool;
-
     private java.util.List<Filter> beforeFilter;
-
     private java.util.List<Filter> afterFilter;
-
     private java.util.List<Rule> rules;
-
-    private java.util.Map<String,Value> varpool;
-
-    private transient java.util.List<UserInput> neededUserInput;
-
+    private java.util.Map<String, Value> varpool;
+    private java.util.List<UserInput> neededUserInput;
     private String name;
-
     private String description;
-
     private SymbolAnimator animator;
-
     private java.util.Set<Sensor> preselectedSensors;
-
     private Map<RequiredStep, Boolean> requiredSteps;
-
     private Action defaultAction;
 
-    public SymbolAdapter () {
+    public SymbolAdapter() {
 
         beforeFilter = new ArrayList<Filter>();
         afterFilter = new ArrayList<Filter>();
 
         rules = new ArrayList<Rule>();
 
-        varpool = new java.util.HashMap<String,Value>();
+        varpool = new java.util.HashMap<String, Value>();
 
         neededUserInput = new ArrayList<UserInput>();
 
         preselectedSensors = new java.util.HashSet<Sensor>();
 
-        requiredSteps = new java.util.HashMap<RequiredStep,Boolean>();
+        requiredSteps = new java.util.HashMap<RequiredStep, Boolean>();
     }
 
-
-    public java.util.List<Filter> getAfterFilter () {
+    public java.util.List<Filter> getAfterFilter() {
         return afterFilter;
     }
 
-    public SymbolAnimator getAnimator () {
+    public SymbolAnimator getAnimator() {
         return animator;
     }
 
-    public void setAnimator (SymbolAnimator val) {
+    public void setAnimator(SymbolAnimator val) {
         this.animator = val;
     }
 
-    public java.util.List<Filter> getBeforeFilter () {
+    public java.util.List<Filter> getBeforeFilter() {
         return beforeFilter;
     }
 
-    public String getDescription () {
+    public String getDescription() {
         return description;
     }
 
-    public void setDescription (String val) {
+    public void setDescription(String val) {
         this.description = val;
     }
 
-    public String getName () {
+    public String getName() {
         return name;
     }
 
-    public void setName (String val) {
+    public void setName(String val) {
         this.name = val;
     }
 
-    public java.util.List<UserInput> getNeededUserInput () {
+    public java.util.List<UserInput> getNeededUserInput() {
         return neededUserInput;
     }
 
-    public java.util.List<Rule> getRules () {
+    public java.util.List<Rule> getRules() {
         return rules;
     }
 
-    public ActionTool getTool () {
+    public ActionTool getTool() {
         return tool;
     }
 
@@ -108,55 +100,77 @@ public class SymbolAdapter implements Serializable{
      *        test doc
      *      </p>
      */
-    public void setTool (ActionTool val) {
+    public void setTool(ActionTool val) {
         this.tool = val;
     }
 
-    public java.util.Map<String,Value> getVarpool () {
+    public java.util.Map<String, Value> getVarpool() {
         return varpool;
     }
 
-    public Object clone () {
-        return null;
+    public SymbolAdapter createCopy() {
+        SymbolAdapter copy = null;
+
+        try {
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            ObjectOutputStream os = new ObjectOutputStream(out);
+
+            os.writeObject(this);
+            os.flush();
+
+            ByteArrayInputStream in = new ByteArrayInputStream(out.toByteArray());
+            ObjectInputStream is = new ObjectInputStream(in);
+
+            copy = (SymbolAdapter) is.readObject();
+
+            is.close();
+            os.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return copy;
     }
 
-    public Action getDefaultAction () {
+    public Action getDefaultAction() {
         return defaultAction;
     }
 
-    public void setDefaultAction (Action val) {
+    public void setDefaultAction(Action val) {
         this.defaultAction = val;
     }
 
-    public java.util.Set<Sensor> getPreselectedSensors () {
+    public java.util.Set<Sensor> getPreselectedSensors() {
         return preselectedSensors;
     }
 
-    public Map<RequiredStep, Boolean> getRequiredSteps () {
+    public Map<RequiredStep, Boolean> getRequiredSteps() {
         return requiredSteps;
     }
 
-    public void save (ObjectOutputStream os) {
-    }
-
+    /*public void save (ObjectOutputStream os) {
+    }*/
     public String toString() {
         //returns name to be displayed in Animation list
         return this.name;
     }
 
-    public void execute () {
-        for (Filter bf : getBeforeFilter()){
+    public void execute() {
+        for (Filter bf : getBeforeFilter()) {
             bf.doFilter();
         }
 
-        for (Rule r : getRules()){
-            if (r.tryExecute()) break;
+        for (Rule r : getRules()) {
+            if (r.tryExecute()) {
+                break;
+            }
         }
 
-        for (Filter af : getAfterFilter()){
+        for (Filter af : getAfterFilter()) {
             af.doFilter();
         }
     }
-
 }
 
