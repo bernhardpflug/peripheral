49286091f23db5118f36bae16a2670edb1f7134f
a.sippl@gmx.at <a.sippl@gmx.at@40046408-4061-11de-9687-fb973330207e>
2009-05-29 13:50:02 +0000

    added type info, i.e. whether the ToolList holds Tools of type Point or Region

--- trunk/peripheral/unstable/src/peripheral/logic/positioningtool/ToolList.java
+++ trunk/peripheral/unstable/src/peripheral/logic/positioningtool/ToolList.java
@@ -2,43 +2,44 @@ package peripheral.logic.positioningtool;
 
 import java.util.ArrayList;
 
-
 public class ToolList implements ActionTool {
 
     private java.util.ArrayList<PositioningTool> visibleElements;
-
     private java.util.ArrayList<PositioningTool> hiddenElements;
+    private Class type;
 
-    public ToolList () {
-        visibleElements = new ArrayList<PositioningTool>();
-        hiddenElements = new ArrayList<PositioningTool>();
+    public ToolList(Class type) {
+        this.type = type;
+
+        if (type.equals(Point.class) || type.equals(Region.class)) {
+            visibleElements = new ArrayList<PositioningTool>();
+            hiddenElements = new ArrayList<PositioningTool>();
+        }
+        else{
+            throw new IllegalArgumentException("type must be one of Point.class or Region.class");
+        }
     }
 
-    public java.util.ArrayList<PositioningTool> getHiddenElements () {
+    public java.util.ArrayList<PositioningTool> getHiddenElements() {
         return hiddenElements;
     }
 
-    public void setHiddenElements (java.util.ArrayList<PositioningTool> val) {
-        this.hiddenElements = val;
-    }
-
-    public java.util.ArrayList<PositioningTool> getVisibleElements () {
+    public java.util.ArrayList<PositioningTool> getVisibleElements() {
         return visibleElements;
     }
 
-    public void setVisibleElements (java.util.ArrayList<PositioningTool> val) {
-        this.visibleElements = val;
+    public Class getType() {
+        return type;
     }
 
-    public void calculateRelativeCoordinates () {
+    public void calculateRelativeCoordinates() {
     }
 
-    public void draw (java.awt.Graphics g) {
+    public void draw(java.awt.Graphics g) {
     }
 
     public java.util.ArrayList<PositioningTool> getElements() {
         return (ArrayList<PositioningTool>) visibleElements;
     }
-
 }
 
