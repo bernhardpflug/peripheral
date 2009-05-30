b9950e7ef2c03ccdb49b1c97ab181696ebdc1f52
a.sippl@gmx.at <a.sippl@gmx.at@40046408-4061-11de-9687-fb973330207e>
2009-05-30 15:11:55 +0000

    added filter for randomly picking positioning tools out of a toollist added base class filter for randomly choosing values out of a list of values

---
+++ trunk/peripheral/unstable/src/peripheral/logic/filter/RandomPositioningToolPickerFilter.java
@@ -0,0 +1,63 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package peripheral.logic.filter;
+
+import java.util.ArrayList;
+import java.util.BitSet;
+import java.util.List;
+import java.util.Random;
+import peripheral.logic.Logging;
+import peripheral.logic.positioningtool.PositioningTool;
+import peripheral.logic.symboladapter.SymbolAdapter;
+import peripheral.logic.value.ConstValue;
+
+/**
+ *
+ * @author Andi
+ */
+public class RandomPositioningToolPickerFilter extends RandomValuePickerFilter {
+    public RandomPositioningToolPickerFilter(SymbolAdapter adapter, String outputVarName) {
+        super(adapter, outputVarName);
+
+        List<PositioningTool> value = new ArrayList<PositioningTool>();
+
+        FilterInput fi = new FilterInput("valueList", new Class[]{value.getClass()});
+        filterInputs.put(fi.getName(), fi);
+
+        fi = new FilterInput("nrToPick", new Class[]{Integer.class});
+        filterInputs.put(fi.getName(), fi);
+
+        outputValue = new ConstValue(adapter, outputVarName, value, value.getClass());
+    }
+
+    @Override
+    public void doFilter() {
+        List<PositioningTool> valueList = (List<PositioningTool>) getFilterInputValue("valueList");
+        int nrToPick = (Integer) getFilterInputValue("nrToPick");
+
+        List<PositioningTool> pickedValues;
+
+        //if nrToPick >= valueList.size() then all values have to
+        //be returned
+        if (nrToPick >= valueList.size()){
+            pickedValues = new ArrayList<PositioningTool>(valueList);
+        }
+        else{
+            pickedValues = new ArrayList<PositioningTool>();
+
+            int [] pickedIndizes = this.getPickedIndizesNumbers(nrToPick, false, 0, valueList.size()-1);
+
+            for (int i : pickedIndizes){
+                pickedValues.add(valueList.get(i));
+            }
+        }
+
+        outputValue.setValue(pickedValues);
+
+        Logging.getLogger().finer("valueList=" + valueList + "; nrToPick=" + nrToPick + "; pickedValues=" + pickedValues);
+    }
+
+}
