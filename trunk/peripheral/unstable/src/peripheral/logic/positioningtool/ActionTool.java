e68278df4aa05cdcd7300e9dfac5c01d2ad9025e
a.sippl@gmx.at <a.sippl@gmx.at@40046408-4061-11de-9687-fb973330207e>
2009-05-31 12:17:13 +0000

    some small changes + change in toollist: removed lists visibleElements/hiddenElements --> now hold values in list elements

--- trunk/peripheral/unstable/src/peripheral/logic/positioningtool/ActionTool.java
+++ trunk/peripheral/unstable/src/peripheral/logic/positioningtool/ActionTool.java
@@ -1,7 +1,7 @@
 package peripheral.logic.positioningtool;
 
 import java.io.Serializable;
-import java.util.ArrayList;
+import java.util.List;
 
 
 public interface ActionTool extends Serializable{
@@ -10,7 +10,7 @@ public interface ActionTool extends Serializable{
 
     public void draw (java.awt.Graphics g);
 
-    public ArrayList<PositioningTool> getElements();
+    public List<PositioningTool> getElements();
 
 }
 
