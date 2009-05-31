6ffcc47720a0a3302d3431e6892674241173511d
bernhard.pflug@gmail.com <bernhard.pflug@gmail.com@40046408-4061-11de-9687-fb973330207e>
2009-05-31 11:41:31 +0000

    added BackgroundImage as File instance to DisplayConfiguration

--- trunk/peripheral/unstable/src/peripheral/designer/DesignerGUI.java
+++ trunk/peripheral/unstable/src/peripheral/designer/DesignerGUI.java
@@ -465,6 +465,7 @@ public class DesignerGUI extends javax.swing.JFrame {
             if (selectedFile != null && PreviewDialog.isExtentionSupported(selectedFile)) {
 
                 DisplayConfiguration.getInstance().setBackgroundImage(PreviewDialog.getInstance().getBackgroundImage());
+                DisplayConfiguration.getInstance().setBackgroundImageFilename(selectedFile);
 
                 currentIndex++;
                 cl.next(cardPanel);
